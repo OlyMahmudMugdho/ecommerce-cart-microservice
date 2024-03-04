b863415fa2f331d45d2a8d356bdb73a9bda181a0
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-05 00:11:38 +0600

    db connected

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
+import bodyParser from "body-parser";
+import prisma from "./prisma/prisma.js";
 dotenv.config();
 
 const app = express();
-
 app.use(cors());
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(morgan("tiny"));
 const PORT = 3000;
 
@@ -19,6 +23,13 @@ app.get("/", (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.listen(PORT, async () => {
+    try {
+        await prisma.$connect();
+        console.info("Connected to database");
+        console.info(`Server running on port ${PORT}`);
+        await prisma.$disconnect();
+    } catch (error) {
+        console.error(error);
+    }
 });
