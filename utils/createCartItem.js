2dcaa2a52a751f83cfaabead4f3630973fd1f99a
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-19 21:52:34 +0600

    create cart item

--- utils/createCartItem.js
+++ utils/createCartItem.js
@@ -1,3 +1,19 @@
 import prisma from "../prisma/prisma";
 
-
+const createCartItem = async (productId,cartId, name, price, quantity) => {
+    try {
+        const item = await prisma.cartItem.create({
+            data : {
+                productId,
+                name,
+                price,
+                quantity,
+                cartId,
+            }
+        })
+        return item;
+    } catch (error) {
+        console.log(error)
+        return null;
+    } 
+}
