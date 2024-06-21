c419ff05b1c31e918b6f9fb37a8a4edfa6ee2d99
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-21 12:18:02 +0600

    take params

--- controllers/removeFromCart.controller..js
+++ controllers/removeFromCart.controller..js
@@ -1,8 +1,7 @@
 import prisma from "../prisma/prisma.js";
 
 const removeFromCart = async (req, res) => {
-    const {userId} = req.params;
-    const {productId} = req.params;
+    const {userId, productId} = req.params;
     console.log(productId)
     return res.status(200).json(userId)
 }
