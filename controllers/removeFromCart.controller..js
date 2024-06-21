d0de6c676ae351f49c535f0b79edfeb48f4b16ca
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-21 12:14:14 +0600

    remove route

--- controllers/removeFromCart.controller..js
+++ controllers/removeFromCart.controller..js
@@ -1,6 +1,9 @@
 import prisma from "../prisma/prisma.js";
 
-export const removeItem = async (req, res) => {
+const removeFromCart = async (req, res) => {
     const {userId} = req.params;
+    const {productId} = req.params;
+    console.log(productId)
     return res.status(200).json(userId)
 }
+export default removeFromCart
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import addToCart from "../controllers/addToCart.controller.js";
-
+import removeFromCart from "../controllers/removeFromCart.controller..js";
 const router = express.Router();
 
 router.route('/add')
     .post(addToCart)
+
+router.route('/remove/:userId/:productId')
+    .delete(removeFromCart)
 
 
 export default router;
