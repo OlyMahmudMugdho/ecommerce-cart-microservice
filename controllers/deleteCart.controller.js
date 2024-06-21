d0e66a051f2ff777e961ce8aa482c6a3a50db84b
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-21 14:11:53 +0600

    delete cart controller

---
+++ controllers/deleteCart.controller.js
@@ -0,0 +1,26 @@
+import prisma from "../prisma/prisma.js";
+
+export const deleteCart = async (req,res) => {
+    const {cartId} = req.params;
+    try {
+        const cart = await prisma.cart.delete({
+            where : {
+                id : cartId
+            }
+        })
+        return res.status(200).json({
+            "success" : true,
+            "ok" : true,
+            "deleted" : true,
+            "message" : "deleted",
+            "data" : cart
+        })
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            "ok" : false,
+            "success" : false,
+            "message" : "internal server error"
+        })
+    }
+}
