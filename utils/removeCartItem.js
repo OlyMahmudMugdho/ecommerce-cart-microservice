89379ed10ff41c0b598b5414f319b41e7e508b31
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-21 12:36:33 +0600

    remove cart item method

---
+++ utils/removeCartItem.js
@@ -0,0 +1,36 @@
+import prisma from "../prisma/prisma"
+
+
+export const removeCartItem = async (userId, productId) => {
+    const cartItem = await prisma.cartItem.findFirst({
+        where: {
+            userId: userId,
+            productId: productId
+        }
+    })
+
+    if (!cartItem) {
+        return null
+    }
+
+    const reducedPrice = await parseFloat(cartItem.price * cartItem.quantity);
+
+    const removedCartItem = await prisma.cartItem.delete({
+        where: {
+            userId: userId,
+            productId: productId
+        }
+    })
+    console.log(removedCartItem)
+    
+    const updatedCart = await prisma.cart.update({
+        where : {
+            userId : userId
+        },
+        data : {
+            totalPrice : parseFloat(totalPrice - reducedPrice)
+        }
+    })
+
+    return await updatedCart;
+}
