8931d722c5944333be4facf252ddd54d9249fa59
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-21 12:42:40 +0600

    schema update

--- controllers/removeFromCart.controller..js
+++ controllers/removeFromCart.controller..js
@@ -1,8 +1,9 @@
-import prisma from "../prisma/prisma.js";
+
+import { removeCartItem } from "../utils/removeCartItem.js";
 
 const removeFromCart = async (req, res) => {
-    const {userId, productId} = req.params;
-    console.log(productId)
-    return res.status(200).json(userId)
+    const { userId, productId } = req.params;
+    const updatedCart = await removeCartItem(userId, productId)
+    return res.status(200).json(await updatedCart)
 }
 export default removeFromCart
--- utils/removeCartItem.js
+++ utils/removeCartItem.js
@@ -1,4 +1,4 @@
-import prisma from "../prisma/prisma"
+import prisma from "../prisma/prisma.js"
 
 
 export const removeCartItem = async (userId, productId) => {
