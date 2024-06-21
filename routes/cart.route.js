bb91156a7b10626b51da0e146688e6999f85f3f0
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-21 14:16:46 +0600

    cascade on delete

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 import addToCart from "../controllers/addToCart.controller.js";
 import removeFromCart from "../controllers/removeFromCart.controller..js";
+import { deleteCart } from "../controllers/deleteCart.controller.js";
 const router = express.Router();
 
 router.route('/add')
     .post(addToCart)
+
+router.route('/delete/:cartId')
+    .delete(deleteCart)
 
 router.route('/remove/:userId/:cartItemId')
     .delete(removeFromCart)
