2dd9dc48fb950b1e8b2063016f17a81291af3bc5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-22 12:30:12 +0600

    cartInfoFromUserId

---
+++ controllers/CartInfoFromUserId.controller.js
@@ -0,0 +1,33 @@
+import { getCartInfoFromUserId } from "../utils/getCartInfo"
+
+
+export const cartInfoFromUserId = async (req, res) => {
+    const { userId } = req.params
+
+    if (!userId) {
+        return res.status(404).json({
+            "error": true,
+            "message": "user id or cart id is required"
+        })
+    }
+
+    try {
+        const cart = await getCartInfoFromUserId(userId);
+        if (!cart) {
+            return res.status(404).json({
+                "error": true,
+                "message": "you have not created any cart"
+            })
+        }
+        return res.status(200).json({
+            "ok": true,
+            "success": true,
+            "data": cart
+        })
+    } catch (error) {
+        return res.status(500).json({
+            "error": true,
+            "message": "internal server error"
+        })
+    }
+}
