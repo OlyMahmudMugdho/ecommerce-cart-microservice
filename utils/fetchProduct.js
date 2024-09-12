f04bfb5014f31ef81fbf4ec8f858a3d8067893b0
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-12 13:31:30 +0600

    update host and port

--- utils/fetchProduct.js
+++ utils/fetchProduct.js
@@ -1,5 +1,5 @@
 export const fetchProduct = async (id) => {
-    const url = `http://localhost:8080/api/v1/products/${id}`;
+    const url = `http://ims-app:8081/api/v1/products/${id}`;
     const response = await fetch(url);
     const product = await response.json();
     return product;
